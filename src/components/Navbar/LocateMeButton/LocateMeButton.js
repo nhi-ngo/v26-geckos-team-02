81db97564de1dad7845d230de74cd7f84d063448
spicysos <cchae216@gmail.com>
2021-01-16 18:36:35 -0500

    added a locate me button - fetches longitude and latitude upon clicking on the button

--- src/components/Navbar/LocateMeButton/LocateMeButton.js
+++ src/components/Navbar/LocateMeButton/LocateMeButton.js
@@ -1,5 +1,40 @@
 import React from "react";
+import Button from '@material-ui/core/Button';
 
 export default function LocateMeButton() {
-  return <div>[Locate me button]</div>;
+  // const fetchLocationName = async (lat,lng) => {
+  // };
+
+  function onSuccess(pos) {
+    console.log("you running..")
+    let crd = pos.coords;
+    console.log(`More or less ${crd.accuracy} meters.`);
+    let lat = crd.latitude.toString(); 
+    let lng = crd.longitude.toString(); 
+    //var coordinates = [lat, lng]; 
+    console.log('Your current position is:');
+    console.log(`Latitude: ${lat}, Longitude: ${lng}`); 
+    //fetchLocationName(lat, lng);
+
+  }
+  
+  function onError(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+    alert("Geolocation is not supported by your browser.")
+  }
+
+  const clickLocateMe = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    } else {
+        alert("Geolocation is not supported by your browser.");
+    }
+  }
+
+
+  return <div>
+    <Button variant="contained" color="primary" onClick={() => clickLocateMe()}>
+      Locate Me
+    </Button>
+  </div>;
 }
