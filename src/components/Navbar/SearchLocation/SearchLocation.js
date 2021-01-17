2b996ee812ffde206e238941bc1fbdb0bbe2d2c8
nhi-ngo <nhingo.y@gmail.com>
2021-01-17 13:51:49 -0500

    replace 'disabled' with 'hidden' attribute

--- src/components/Navbar/SearchLocation/SearchLocation.js
+++ src/components/Navbar/SearchLocation/SearchLocation.js
@@ -19,11 +19,7 @@ export default function SearchLocation() {
   return (
     <div>
       <select id="states">
-<<<<<<< HEAD
         <option value="" hidden>
-=======
-        <option value="" disabled>
->>>>>>> 81bbeab34aa5276b374a2f1a1685d0dd9be6cd1b
           Select a state
         </option>
         {states.map(state => (
