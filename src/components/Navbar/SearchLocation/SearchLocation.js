906d5d35c66ab42a828b329915315ad64a5b10ca
nhi-ngo <nhingo.y@gmail.com>
2021-01-16 21:42:17 -0500

    fetch list of states upon page loading

--- src/components/Navbar/SearchLocation/SearchLocation.js
+++ src/components/Navbar/SearchLocation/SearchLocation.js
@@ -1,7 +1,30 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
+import data from "./data.json";
 
-export default class SearchLocation extends Component {
-  render() {
-    return <div>[Search location]</div>;
+export default function SearchLocation() {
+  const [states, setStates] = useState([]);
+
+  // const API_KEY = "e14y0OqlWGddCHCXLC3UzLBlcKecsGrxlahVvogx";
+
+  // const stateEl = useRef();
+
+  useEffect(() => {
+    fetchListOfStates();
+  }, []);
+
+  function fetchListOfStates() {
+    setStates(data);
   }
+
+  return (
+    <div>
+      <select id="states">
+        {states.map(state => (
+          <option value={state.abbr} key={state.abbr}>
+            {state.name}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
 }
