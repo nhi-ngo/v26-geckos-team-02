0c35cac129c7279bf32618d4b841d0aec41e440b
nhi-ngo <nhingo.y@gmail.com>
2021-01-20 17:15:03 -0500

    minor prettier fix

--- src/components/Navbar/LocateMeButton/LocateMeButton.js
+++ src/components/Navbar/LocateMeButton/LocateMeButton.js
@@ -1,10 +1,16 @@
 import React from "react";
-import Button from '@material-ui/core/Button';
+import Button from "@material-ui/core/Button";
 
 export default function LocateMeButton() {
-  return <div>
-    <Button variant="contained" color="primary" onClick={() => clickLocateMe()}>
-      Locate Me
-    </Button>
-  </div>;
+  return (
+    <div>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => clickLocateMe()}
+      >
+        Locate Me
+      </Button>
+    </div>
+  );
 }
