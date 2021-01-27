557302dabb6ed0cd88f197e6c758f0547bf77548
spicysos <cchae216@gmail.com>
2021-01-27 13:11:21 -0500

    added date dropdown

--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -3,6 +3,7 @@ import GeolocationInfoBanner from "./GeolocationInfoBanner/GeolocationInfoBanner
 import DataSearchForm from "./DataSearchForm/DataSearchForm";
 import InteractiveMap from "./InteractiveMap/InteractiveMap";
 import DataGraph from "./DataGraph/DataGraph";
+import DateDropDown from "./DateDropdown";
 
 export default class MainContent extends Component {
   constructor(props) {
@@ -11,9 +12,19 @@ export default class MainContent extends Component {
       showGeolocationInfoBanner: true,
       showDataSearchForm: false,
       showFullWidthMap: true,
+      fromYear: 1993,
+      toYear: 2000,
     };
+    this.updateToYear = this.updateToYear.bind(this)
+    this.updateFromYear = this.updateFromYear.bind(this)
   }
-
+  updateToYear(toYear){
+    console.log(toYear)
+    this.setState({ toYear : toYear })
+  }
+  updateFromYear(fromYear){
+    this.setState({ fromYear : fromYear })
+  }
   render() {
     const { showDataSearchForm, showFullWidthMap } = this.state;
 
@@ -24,6 +35,7 @@ export default class MainContent extends Component {
         <h1>Be Save Where You Are!</h1>
 
         {showGeolocationInfoBanner && <GeolocationInfoBanner />}
+        <DateDropDown updateToYear={this.updateToYear} updateFromYear={this.updateFromYear} fromYear={this.state.fromYear} toYear={this.state.toYear}/>
         {showDataSearchForm && <DataSearchForm />}
         <InteractiveMap fullWidth={showFullWidthMap} />
 
